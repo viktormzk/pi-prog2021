c562ed7cc29aba496507d3979cbd35abf1a2a1ea
Victor <muzyka.viktor@elitar.rv.ua>
2021-01-28 14:06:42 +0200

    Interactive mode

--- 1/1.cpp
+++ 1/1.cpp
@@ -40,7 +40,7 @@ int main()
 	//char new_name[100];
 	cout << "Select the operating mode: 1 Interactive mode, 2 Demonstration mode, 3 Benchmark mode,9 EXIT: "<<endl;
 	cin >> mode;
-	while (mode!=9){
+	//while (mode!=9){
 	if (mode==1){
 		cout << "Select the operating mode: 1 Adding item, "<<
 		"2 Data storage, 3 Data recovery, 4 Output of all saved data, "<<
@@ -48,28 +48,28 @@ int main()
 		"6 Modification of elements, 7 Delete items, 9 Exit" << endl;
 		while (interactive!=9){
 		cin >> interactive;
-		
 		if (interactive==1) {
 			cout << "Write number of items: ";
 			cin >> numb;
-			for (i=ind;i<ind+numb;i++){
-				cin >> out[i].id >> out[i].name >>out[i].uom >> out[i].num >> out[i].data.hour
-				>> out[i].data.min >> out[i].data.day >> out[i].data.month>>
-				out[i].data.year>> out[i].term;
+			for (i=0;i<numb;i++){
+				cin >> f[i].id >> f[i].name >>f[i].uom >> f[i].num >> f[i].data.hour
+				>> f[i].data.min >> f[i].data.day >> f[i].data.month>>
+				f[i].data.year>> f[i].term;
  			/*	words[i]=out[i].id +" "+ out[i].name +" "+ out[i].uom+" "+to_string(out[i].num) +" "+to_string(out[i].data.hour)
 				+" "+ to_string(out[i].data.min) +" "+to_string(out[i].data.day) +" "+ to_string(out[i].data.month)+" "+
 				to_string(out[i].data.year)+" "+ to_string(out[i].term) ; */} 
 				}
 				
 		if (interactive==2){
-		fout_bin=fopen("products_bin.dat", "a"/*, "ab+"*/);
-		ofstream fout("products.txt"/*, ios_base::app*/); 
+		fout_bin=fopen("products_bin.dat", "ab+");
+		ofstream fout("products.txt", ios_base::app); 
 		fout << endl;
-			for (i=0;i<numb+ind;i++){
-				words[i]=out[i].id +" "+ out[i].name +" "+ out[i].uom+" "+to_string(out[i].num) +" "+to_string(out[i].data.hour)
-				+" "+ to_string(out[i].data.min) +" "+to_string(out[i].data.day) +" "+ to_string(out[i].data.month)+" "+
+			for (i=0;i<numb;i++){
+			words[i]=f[i].id +" "+ f[i].name +" "+ f[i].uom+" "+to_string(f[i].num) +" "+to_string(f[i].data.hour)
+				+" "+ to_string(f[i].data.min) +" "+to_string(f[i].data.day) +" "+ to_string(f[i].data.month)+" "+
 				to_string(out[i].data.year)+" "+ to_string(out[i].term) ; 
-				fout << words[i] << endl;
+					if (i!=numb-1) 	fout << words[i] << endl;
+						else fout << words[i];
 				//fwrite("\n",sizeof(char),1, fout_bin);	
 					for (j=0;j<words[i].length();j++) 					
 						fwrite((char*)&words[i][j],sizeof(char),1, fout_bin);
@@ -81,6 +81,7 @@ int main()
 		
 		if (interactive==3){
 		ifstream file("products.txt"); 
+		ind=0;
 		/*int n=100;
 		char *buffer=new char[n+1];
 		buffer[n]=0;*/ 
@@ -101,9 +102,8 @@ int main()
 		
 		if (interactive==4){
 			for(i=0;i<ind;i++){
-				if (out[i].term<=3650 && out[i].term>0)
 				cout << line[i]<<endl;
-			}
+			} 
 		}
 		
 		if (interactive==5){
@@ -133,13 +133,14 @@ int main()
 					&& out[i].data.day>=day	&& out[i].data.hour>=hour && out[i].data.min>minutes )) flag=true;
 					//cout << new_name << endl;
 					if (new_name==str_name && out[i].num>min && out[i].num<max 
-					&& out[i].uom == str_uom && flag) cout << line [i] << endl;
+					&& out[i].uom == str_uom && flag==true) {cout << line [i] << endl;
+				 		flag=false;}
 				}	
 				
 				
 		   }
 		   
-		   if (interactive==6) {
+		   /*if (interactive==6) {
 		   	// Everything was fine until I started writing modifications...
 		   	string name, categ, value;
 		   	int spec=-1;
@@ -161,11 +162,11 @@ int main()
 			  		 if (categ=="term") out[spec].term=trash;}} 
 					   else cout <<"ERROR NAME"<<endl;
 					   
-		   }
+		   }*/
 		   
 		   } }
 	
 	
-}
+
 return 0;     
 }
