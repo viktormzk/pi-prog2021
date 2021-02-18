5723fe190f42ca1747ccc02dec8625b5159a3de9
Victor <viktormuzyka@knu.ua>
2021-02-18 21:03:34 +0200

    Add interactive and demo mode

--- Lab_2/Source.cpp
+++ Lab_2/Source.cpp
@@ -69,8 +69,10 @@ ListNode* append(circle data) {
 bool remove(int index_to_remove) {
 	if (index_to_remove < 0){
 		index_to_remove+=size;
+		//std::cout << "OK" <<std::endl;
 	}	
 	if (index_to_remove >= size || index_to_remove<0) {
+		 std::cout << "Not ok, error index" <<std::endl;
 			return false;
 	} 
 		bool from_begin = false;
@@ -95,8 +97,8 @@ bool remove(int index_to_remove) {
 		}
 		return false;
 	}
-void remove_if(list &my_list,circle x){
-	ListNode* current=my_list.search(x);
+void remove_if(list &my_list, circle x){
+	ListNode* current=my_list.search(x); //add new func search by value
 	current = remove_node(current);	
 }
 private:
@@ -117,15 +119,17 @@ private:
 	}
 };
 void show(list &my_list ) {
+	std::cout << std::endl;
+	std::cout << "List: " << std::endl;
 		ListNode* current = my_list.begin;
 		while (current) {
 			std::cout<<current->data.x<<" "<<current->data.y <<" " << current->data.r<<std::endl;
 			current = current->next;
 		}
-		std::cout<<std::endl;
+		std::cout << std::endl;
 	}
 	void length(list &my_list)	{
-	std::cout << my_list.size << std::endl;	
+	std::cout <<"Length: "<< my_list.size << std::endl;	
 	}
 	
 bool insert(list &my_list, circle x, int pos)
@@ -172,30 +176,89 @@ void create_empty(list &my_list)
 	
 int main(){
 	list my_list;
-	int number=-2;
-	std::cout << "	Test empty list" << std::endl;
-	create_empty(my_list);
-	show(my_list);
-	std::cout << "	Test add1" << std::endl;
-	create_empty(my_list);
-	my_list.append({1,1,1});
-	my_list.append({2,4,2});
-	show(my_list);
-	std::cout << "	Test insert" << std::endl;
-	create_empty(my_list);
-	my_list.append({1,1,1});
-	my_list.append({2,4,2}); 
-	my_list.append({12,11,31});
-	my_list.append({232,412,212});
-	insert(my_list,{1,5,7}, 1);
-	show(my_list);
-	std::cout << "	Test length" << std::endl;
-	length(my_list);
-	std::cout << "	Test get " <<number<< std::endl;
-	my_list.get(number);
-	std::cout << "	Test remove " <<number<< std::endl;
-	my_list.remove_if(my_list, {12,11,31});
-	show(my_list);
+	int mode=1,interactive=1;
+	std::cout << "Select the operating mode: 1 - Demo mode, 2 - Interactive mode, 3 - Benchmark "<<std::endl;
+	std::cin >> mode;
+	if (mode==1){
+		std::cout << "Enter 1 - Create empty list, 2 - Add item to end, 3 - Insert item, "<<
+		"4 - Remove by index, 5 - Show length of list, 6 - Show list, 7 - Remove by value, 8 - Get by index, 9 - Exit"<< std::endl;
+		int number=-1;
+		std::cout << "1" << std::endl;
+		std::cout << "6" << std::endl;
+		create_empty(my_list);
+		show(my_list);
+		std::cout << "1" << std::endl;
+		std::cout << "2 \n1 1 1 \n2 \n2.23 4.12 2.111" << std::endl;
+		std::cout << "6" << std::endl;
+		create_empty(my_list);
+		my_list.append({1,1,1});
+		my_list.append({2.23,4.12,2.111});
+		show(my_list);
+		std::cout << "1" << std::endl;
+		std::cout << "2 \n1 1 1 \n2\n2 4 2 \n2\n12 11 31 \n2\n232 412 212 \n3 \n1 5 7 1" << std::endl;
+		std::cout << "6" << std::endl;
+		create_empty(my_list);
+		my_list.append({1,1,1});
+		my_list.append({2,4,2}); 
+		my_list.append({12,11,31});
+		my_list.append({232,412,212});
+		insert(my_list,{1,5,7}, 1);
+		show(my_list);
+		std::cout << "5 "<<std::endl;
+		length(my_list);
+		std::cout << std::endl;
+		std::cout << "8 \n" <<number<< std::endl;
+		my_list.get(number);
+		std::cout << "4 \n" <<number<< std::endl;
+		std::cout << "6" << std::endl;
+		my_list.remove(number);
+		show(my_list);
+		std::cout << "7 \n1 1 1" << std::endl;
+		std::cout << "6" << std::endl;
+		my_list.remove_if(my_list, {1,1,1});
+		show(my_list);
+		} else
+	if (mode==2) {
+		std::cout << "Enter 1 - Create empty list, 2 - Add item to end, 3 - Insert item, "<<
+		"4 - Remove by index, 5 - Show length of list, 6 - Show list, 7 - Remove by value, 9 - Exit"<< std::endl;
+		while (interactive!=9){
+			std::cin >> interactive;
+			if (interactive==1){
+				create_empty(my_list);
+			} else
+			if (interactive==2){
+				double x,y,r;
+				std::cout << "Write coordinates and radius: "<<std::endl;
+				std::cin >> x >> y >> r;
+				my_list.append({x,y,r});
+			} else
+			if (interactive==3){
+				double x_ins,y_ins,r_ins;
+				int ind;
+				std::cout << "Write coordinates, radius and index of insert "<<std::endl;
+				std::cin >> x_ins >> y_ins >> r_ins >> ind;
+				insert(my_list,{x_ins,y_ins,r_ins}, ind);
+			} else
+			if (interactive==4){
+				int index;
+				std::cout << "Write index for remove: "<<std::endl;
+				my_list.remove(index);
+			}else
+			if (interactive==5){
+				length(my_list);
+			} else
+			if (interactive==6){
+				show(my_list);	
+			}else
+			if (interactive==7){
+				circle rem;
+				std::cout << "Write value for remove: "<<std::endl;
+				std::cin >> rem.x >> rem.y >> rem.r;
+				my_list.remove_if(my_list, rem);
+			}
+		}
+	}
+	
 	//I note just for fun :)
 	//17.02.21 15:37
 	//18.02.21 00:33
@@ -204,7 +267,7 @@ int main(){
 	//18.02.21 07:51
 	
 	//18.02.21 18:51
-	//18.02.21 19:51
+	//18.02.21 21:02
 	
 	return 0;
 }
