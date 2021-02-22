7ac8f726d97841ef785a87e907706b627690bb55
Victor <viktormuzyka@knu.ua>
2021-02-22 15:08:24 +0200

    Delete all global variables

--- Lab_1/Source.cpp
+++ Lab_1/Source.cpp
@@ -10,17 +10,15 @@
 #include <set>
 #include <stdlib.h>
 #include <ctime>
+using namespace std;
 //#include <filesystem>
 //#include <sqlite3.c>
 
  //database include
  #include "sqlite3.h"
-int data_id = 0,memory=0;
-unsigned int time_bin_start=0, time_bin_end=0, time_bin=0, time_str_end=0, time_str=0,  time_str_start=0;
 //for sqlite
-using namespace std;
-const char* SQL = "CREATE TABLE IF NOT EXISTS foo(a char(100)); INSERT INTO FOO VALUES(\"id-1 Carrot kilo 123 12 12 13 1 2020 52\");";
-string sql_str;
+/*const char* SQL = "CREATE TABLE IF NOT EXISTS foo(a char(100)); INSERT INTO FOO VALUES(\"id-1 Carrot kilo 123 12 12 13 1 2020 52\");";
+string sql_str;*/
 
 
 
@@ -66,7 +64,7 @@ string to_string(int n)
 	sprintf(buf, "%d", n);
 	return buf;
 }
-int index=0,ind=0,ind_my_line=0;
+
 //for reading from bin file
 /*vector<product> a[200000];
 
@@ -114,7 +112,7 @@ int fileSize_bin(const char* add) {
 	return size;
 }
 //generation
-void func(int gen,int mode) {
+void func(int gen,int mode, unsigned int &time_bin,unsigned int  &time_str,int &data_id,int &memory,int k) {
 	FILE* fout_bin;
 	int i, j;
 	string* words = new string[gen+1];
@@ -129,6 +127,7 @@ void func(int gen,int mode) {
 "herring","kipper","mackerel","pilchard","plaice","salmon","sardine",
 "sole","trout","tuna","artichoke","asparagus","ubergine","avocado",
 "beansprouts","beetroot","broccoli","cabbage","carrot","cauliflower","celery" }, units[4] = { "kilo", "litr", "pieces" };
+unsigned int time_bin_start=0, time_bin_end=0, time_str_end=0,  time_str_start=0;
 	if (mode==1){
 	fout_bin = fopen("products_bin.dat", "wa+");
 	fout.open("products.txt");
@@ -156,12 +155,14 @@ void func(int gen,int mode) {
 			+ " " + to_string(f[i].data.min) + " " + to_string(f[i].data.day) + " " + to_string(f[i].data.month) + " " +
 			to_string(f[i].data.year) + " " + to_string(f[i].term);
 		//cout << sizeof(f[i]) << endl;
-		memory+=words[i].size()*2;	
+		memory+=words[i].size();	
 		time_str_start=clock();
 		// ofstream
-		if (mode==1){
-			if (i==gen-1) fout << words[i];
+		if (mode==1 && k==1){
+			if (i==gen) fout << words[i];
 			else fout << words[i] << endl;
+		} else if (mode==1 && k==0){
+			fout << words[i] << endl;
 		} else
 		fout << words[i] << endl;
 		
@@ -201,11 +202,8 @@ void func(int gen,int mode) {
 	delete[] words, f;
 }
 
-string* words = new string[200000],* line = new string[200000],*my_line = new string[200000];
-
-int numb;
 //adding for interactive
-void add_items(int numb){
+void add_items(int numb,string* words){
 	int i=0;
 		if (numb>199990){
 					cout << "In main up to 200k products, u can change "<<endl;
@@ -227,7 +225,7 @@ void add_items(int numb){
 				} 
 }
 //saving for interactive
-void save_items(){
+void save_items(string *words,int numb){
 	FILE * fout_bin;
 	fout_bin = fopen("products_bin.dat", "ab+");
 				ofstream fout("products.txt", ios_base::app);
@@ -241,7 +239,7 @@ void save_items(){
 					else fout << words[i];
 					
 					fwrite("\n",sizeof(char),1, fout_bin);
-					for (j = 0;j < words[i].length();j++)
+					for (j = 0;j < words[i].size();j++)
 						fwrite(/*(char*)*/&words[i][j], sizeof(char), 1, fout_bin);
 					//fwrite("\n", sizeof(char), 1, fout_bin);
 
@@ -255,37 +253,34 @@ void save_items(){
 				fclose(fout_bin);
 }
 //recovery data for interactive
-void recovery_items(){
+void recovery_items(string* line,int &index){
 			ifstream file("products.txt");
-				ind = 0;
+				int ind = 0;
 				//if (mode==1) product* out = new product[200000]; else
 				product* out = new product[200000];		
 				/*int n=100;
 				char *buffer=new char[n+1];
 				buffer[n]=0;*/
-				while (!file.eof()) {				
+				while (!file.eof()) {			
 					file >> out[ind].id >> out[ind].name >> out[ind].uom >> out[ind].num >> out[ind].data.hour
 						>> out[ind].data.min >> out[ind].data.day >> out[ind].data.month >>
 						out[ind].data.year >> out[ind].term;
 					//	file.getline(buffer,n);
-					memory+=sizeof(out[ind]);
 					//initfromfile(a[i]);	
-					
 				line[ind] = out[ind].id + " " + out[ind].name + " " + out[ind].uom +
 						" " + to_string(out[ind].num) + " " + to_string(out[ind].data.hour)
 						+ " " + to_string(out[ind].data.min) + " " + to_string(out[ind].data.day) + " " +
 						to_string(out[ind].data.month) + " " + to_string(out[ind].data.year) + " " +
-						to_string(out[ind].term);
-					memory+=line[ind].size();	
+						to_string(out[ind].term);	
 					ind++;
 				} file.close();
-				
+				index=ind;
 			delete [] out;			
 	
 }
-void recovery_benchmark(){
+void recovery_benchmark(int &data_id, int &memory){
 			ifstream file("products.txt");
-				ind = 0;
+				int ind = 0;
 				//if (mode==1) product* out = new product[200000]; else
 				product* out = new product[data_id+1];		
 				/*int n=100;
@@ -307,13 +302,14 @@ void recovery_benchmark(){
 	
 }
 
-void print_items(){
+void print_items(string* &line, int ind){
 	int i;
-	for (i = 0;i < ind;i++) {
+	for (i = 0;i < ind;i++) { 
+	if (line[i][0]=='i')
 					cout << line[i] << endl;
 				}
 }
-void search_items(int mode){
+void search_items(int mode, int &memory, string* line,string* my_line, int ind_my_line){
 	if (mode==1){
 	string str_name,str_uom,new_name;
 	int i=0;
@@ -354,7 +350,7 @@ void search_items(int mode){
 									flag = false;					
 					}
 			 		}
-					file.close();} 
+					file.close();} else
 					if (mode==2){
 					int ind_t = 0;
 						string str_name = "Ca", str_uom = "kilo",new_name,len;
@@ -392,10 +388,13 @@ void search_items(int mode){
 }
 
 int main()
-{
+{ 	string* words = new string[200000],* line = new string[200000],*my_line = new string[200000];
+	int memory=0,data_id=0,id=0;
+	int index=0,ind=0,ind_my_line=0;
+	int numb=0;
 	//product* f = new product[200000];
 	//product* out = new product[200000];
-	int mode = 1, interactive = 0, i, j, k, ind = 0, gen = 10;
+	int mode = 1, interactive = 0, i, j, k, gen = 10;
 	FILE* fout_bin;
 	string str_name, str_uom, new_name;
 	//for clear database
@@ -412,7 +411,9 @@ int main()
 	cin >> mode;
 	// interactive
 	if (mode == 1) {
-		func(gen,1);
+		unsigned int x,y;
+		int id=0;
+		func(gen,1,x,y,id,memory,1);
 		cout << "Select the operating mode: 1 Adding item, " <<
 			"2 Data storage, 3 Data recovery, 4 Output of all saved data, " <<
 			"5 Search by particle of name, 9 EXIT. " << endl;
@@ -422,23 +423,23 @@ int main()
 			if (interactive == 1) {
 				cout << "Write number of items: ";
 				cin >> numb;
-				add_items(numb);
+				add_items(numb, words);
 				}else
 
 			if (interactive == 2) {
-				save_items();	
+				save_items(words, numb);	
 			} else
 
 			if (interactive == 3) {
-				recovery_items();
+				recovery_items(line, ind);
 			}  else
 
 			if (interactive == 4) {
-				print_items();
+				print_items(line,ind);
 			} else
 
 			if (interactive == 5) {
-				search_items(1);
+				search_items(1,memory,line,my_line,ind_my_line);
 			} 
 			}
 			 } else
@@ -447,7 +448,9 @@ int main()
 		// demo
 
 		if (mode == 2) {
-			func(gen,1);
+			unsigned int x,y;
+			id=0;
+			func(gen,1,x,y,id,memory,0);
 			cout << "Select the operating mode: 1 Adding item, " <<
 				"2 Data storage, 3 Data recovery, 4 Output of all saved data, " <<
 				"5 Search by particle of name, " <<
@@ -566,19 +569,18 @@ int main()
 				 start_s_time = 0, start_g_time = 0, start_r_time = 0, end_s_time = 0, end_g_time = 0, end_r_time = 0;
 				//memory = 0;
 				int key;
+				unsigned int time_bin=0,time_str=0;
 				long sum_gen;
 					cout << "Write your number: ";
 					cin >> key;
 
-				//for (key = 1;key <= 10;key++) {
+			//	for (key = 1;key <= 10;key++) {
 					//for clear database
 					/*sql_str = "DELETE FROM foo;";
 					const char* SQL = sql_str.c_str();
 					database(SQL);*/
-					sum_gen = 0;
-					
-					time_str=0;
-					time_bin=0;
+					sum_gen = 0;			
+					memory=0;
 					sum_time = 0;
 					s_time = 0;
 					g_time = 0;
@@ -590,7 +592,7 @@ int main()
 						
 						// generation
 						start_s_time = clock();
-						if (gen == key) func(gen,1); else func(gen,2);
+						if (gen == key) func(gen,1, time_bin, time_str, data_id , memory,0); else func(gen,2,time_bin, time_str,data_id,memory,0);
 						end_s_time = clock();
 						s_time += end_s_time - start_s_time;
 						
@@ -598,13 +600,13 @@ int main()
 						sum_gen += gen;
 						// Data recovery
 						start_r_time = clock();
-						recovery_benchmark();
+						recovery_benchmark(data_id, memory);
 						end_r_time = clock();
 						r_time += end_r_time - start_r_time;
 
 						// Search item
 						start_s_time = clock();
-						search_items(2);
+						search_items(2, memory,line,my_line,ind_my_line);
 						end_s_time = clock();
 						s_time = end_s_time - start_s_time;
 						
@@ -614,7 +616,6 @@ int main()
 						
 						// for check time;
 						//cout << search_time <<" "<< gen <<endl;
-
 						gen = gen * 2;
 					}
 					int current_gen = gen / 2;
@@ -628,20 +629,20 @@ int main()
 						sum_gen += current_gen;
 						// generation
 						start_g_time = clock();
-						func(current_gen,2);
+						func(current_gen,2, time_bin, time_str,data_id, memory,0);
 						end_g_time = clock();
 						g_time += end_g_time - start_g_time;
 
 						ind = 0;
 						// Data recovery
 						start_r_time = clock();
-						recovery_benchmark();
+						recovery_benchmark(data_id, memory);
 						end_r_time = clock();
 						r_time += end_r_time - start_r_time;
 
 						// Search item
 						start_s_time = clock();
-						search_items(2);
+						search_items(2,memory,line,my_line,ind_my_line);
 						end_s_time = clock();
 						s_time = end_s_time - start_s_time;
 						
@@ -658,7 +659,7 @@ int main()
 					}
 					//cout << key << endl;
 					//for writing in file
-			/*		ofstream fout("result.txt", ios_base::app);
+				/*	ofstream fout("result_new.txt", ios_base::app);
 				fout << "N: " << key << endl;
 				fout << "Sum_time: " << sum_time << "ms" << endl;
 				fout << "	Time of searching: "<<s_time<<"ms"<<endl;
@@ -685,7 +686,7 @@ int main()
 				cout << "	Time of recovery data: "<<r_time<<"ms"<<endl;
 				cout << "Used memory: " << memory << " bytes"<<endl;
 				cout << "Number of last generation: " << current_gen << endl;
-				cout << "Sum of generation: " << sum_gen << endl;
+				cout << "Sum of generation: " << sum_gen+1 << endl;
 				cout << "Size of File: "<<fileSize("products.txt")<< " bytes"<<endl;
 				cout << "Size of binary File: "<<fileSize_bin("products_bin.dat")<< " bytes"<<endl;
 				cout << endl;
